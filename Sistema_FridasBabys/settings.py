eb141273eabda1432735f7c18bf9132171883745
Alex888DSI <jmejia5679@utm.edu.ec>
2026-01-10 11:54:47 -0500

    Requeriments actualizado

--- Sistema_FridasBabys/settings.py
+++ Sistema_FridasBabys/settings.py
@@ -62,7 +62,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bd_sistema_fb',
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': 'Alex2',
         'HOST': 'localhost',
         'PORT': '5432',
     }
